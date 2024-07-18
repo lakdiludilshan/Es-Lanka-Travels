71ac784c70eca4d4bbddec56305c92cfdef9e328
lakdiludilshan <lakdiludilshan@gmail.com>
2024-07-19 02:04:28 +0530

    authentication done

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,1 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
+async function requireAuth(req, res, next) {
+  try {
+    //read token off the cookies
+    const token = req.cookies.Authorization;
+
+    //decode the token
+    const decoded = jwt.verify(token, process.env.SECRET);
+
+    //check expiration
+    if (Date.now() > decoded.exp) return res.sendStatus(401);
+
+    //find user using decoded sub
+    const user = await User.findById(decoded.sub);
+    if (!user) return res.sendStatus(401);
+
+    //attach user to req
+    req.user = user;
+
+    //continue on
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+}
+
+module.exports = requireAuth;
