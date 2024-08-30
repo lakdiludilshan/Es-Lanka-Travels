6345dd85368a5de6db49d38a7254160c26bd98f9
lakdiludilshan <lakdiludilshan@gmail.com>
2024-08-30 23:45:51 +0530

    add redux persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import axios from "axios";
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
+  </PersistGate>
 );
