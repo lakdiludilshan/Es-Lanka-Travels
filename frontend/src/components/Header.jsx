980c9a6cb9f436b957e4f951876b3d96e991a145
lakdiludilshan <lakdiludilshan@gmail.com>
2024-08-31 17:01:46 +0530

    update header with user data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,51 +1,89 @@
-import React from 'react';
-import { Navbar, TextInput, Button } from "flowbite-react";
+import React from "react";
+import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const path = useLocation().pathname;
-    return (
-        <Navbar className='border-b-2'>
-            <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Es Lanka</span>
-                Travels
-            </Link>
-            <form className='hidden sm:block'>  {/* Hide on small screens, show on medium and larger */}
-                <TextInput 
-                    type='text'
-                    placeholder='Search...'
-                    rightIcon={AiOutlineSearch}
+  const path = useLocation().pathname;
+  const {currentUser} = useSelector((state) => state.user);
+
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Es Lanka
+        </span>
+        Travels
+      </Link>
+      <form className="hidden sm:block">
+        {" "}
+        {/* Hide on small screens, show on medium and larger */}
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        {" "}
+        {/* Show button only on small screens */}
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10" color="gray" pill>
+          <FaMoon />
+        </Button>
+        {currentUser ? (
+            <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+                <Avatar
+                alt="User"
+                img={currentUser.profilePicture}
+                rounded
                 />
-            </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>  {/* Show button only on small screens */}
-                <AiOutlineSearch />
-            </Button>
-            <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10' color='gray' pill>
-                    <FaMoon />
-                </Button>
-                <Link to='/login'>
-                    <Button gradientDuoTone='purpleToBlue' outline>
-                        Sign In
-                    </Button>
+            }
+            >
+                <Dropdown.Header>
+                    <span className="block text-sm">{currentUser.username}</span>
+                    <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+                </Dropdown.Header>
+                <Link to={"/dashboard?tab=profile"}>
+                    <Dropdown.Item>Profile</Dropdown.Item>
                 </Link>
-                <Navbar.Toggle />
-            </div>
-            <Navbar.Collapse>
-                <Navbar.Link active={path === "/"} as={'div'}>
-                    <Link to='/'>Home</Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === "/about"} as={'div'}>
-                    <Link to='/about'>About</Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === "/tours"} as={'div'}>
-                    <Link to='/tours'>Tours</Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-}
+                <Dropdown.Divider />
+                <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+        ):
+    (
+        <Link to="/login">
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
+        </Link>
+    )    
+    }
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/tours"} as={"div"}>
+          <Link to="/tours">Tours</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
