a1b58956def48c2cc1b19f69211a7ea86e8cc96a
lakdiludilshan <lakdiludilshan@gmail.com>
2025-02-17 23:21:23 +0530

    create get post api routes

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const requireAuth = require("../middleware/requireAuth");
-const {createPost} = require("../controllers/postController");
+const {createPost ,getPosts} = require("../controllers/postController");
+
 
 router.post('/create', requireAuth, createPost);
+router.get('/getposts', getPosts);
 
 module.exports = router;
