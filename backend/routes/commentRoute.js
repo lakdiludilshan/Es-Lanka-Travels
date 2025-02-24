8d67c18c09ddcc69a784beeb255bd981806cb202
lakdiludilshan <lakdiludilshan@gmail.com>
2025-02-25 02:19:23 +0530

    add edit funtionality to comments

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const requireAuth = require("../middleware/requireAuth");
 const router = express.Router();
-const {createComment, getPostComments, likeComment} = require("../controllers/commentController");
+const {createComment, getPostComments, likeComment, editComment} = require("../controllers/commentController");
 
 router.post('/create', requireAuth ,createComment);
 router.get('/getpostcomments/:postId', getPostComments);
 router.put('/likecomment/:commentId', requireAuth, likeComment)
+router.put('/editcomment/:commentId', requireAuth, editComment)
 
 module.exports = router;
